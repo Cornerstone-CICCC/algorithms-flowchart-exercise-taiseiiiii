4c7fa595f3427424b38605119ce66f4d3e0d5007
taiseiiiii <isemiya.0509@gmail.com>
2025-09-16 20:36:42 -0700

    complete exercise-4.js

---
+++ js/exercise-4.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 20;
+const c = 30;
+
+if (a + b > c && a + c > b && b + c > a) {
+  console.log("Triangle is possible");
+} else {
+  console.log("Triangle is not possible");
+}
