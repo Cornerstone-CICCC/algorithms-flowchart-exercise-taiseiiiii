270788347cc1581fbf52e35a07831422497e2a92
taiseiiiii <isemiya.0509@gmail.com>
2025-09-16 20:33:22 -0700

    complete exercise-3.js

---
+++ js/exercise-3.js
@@ -0,0 +1,15 @@
+const num1 = 10;
+const num2 = 100;
+const num3 = 30;
+
+let largest = num1;
+
+if (num2 > largest) {
+  largest = num2;
+}
+
+if (num3 > largest) {
+  largest = num3;
+}
+
+console.log(largest);
